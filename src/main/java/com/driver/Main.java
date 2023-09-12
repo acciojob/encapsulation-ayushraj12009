a1d64342e80796215c3ef9237c462cd66713f3d0
ayushraj12009 <ayushraj12009@gmail.com>
2023-09-12 16:45:08 +0530

    error solving

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         RWOnly ayush = new RWOnly();
-        ayush.setSalary(5000);
-        System.out.print(ayush.getSalary());
+        ayush.setName("raj");
+        System.out.print(ayush.getName());
     }
 
 
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,16 +1,19 @@
 package com.driver;
 
 public class RWOnly {
-    private int salary;
+    private String name;
 
     //getter hai
-    public int getSalary() {
-        return salary;
+
+    public String getName() {
+        return name;
     }
+
 
     //setter hai
 
-    public void setSalary(int salary) {
-        this.salary = salary;
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
