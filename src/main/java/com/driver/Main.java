81de80a4c75f8568c9fcadc9d40d2b8ff9fa8403
ayushraj12009 <ayushraj12009@gmail.com>
2023-09-12 16:40:31 +0530

    Question solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly ayush = new RWOnly();
+        ayush.setSalary(5000);
+        System.out.print(ayush.getSalary());
+    }
+
+
+
+
+
+
+
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int salary;
+
+    //getter hai
+    public int getSalary() {
+        return salary;
+    }
+
+    //setter hai
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
